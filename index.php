08e33e030daec8e495a0ec625027a590c8c9ddd4
Daniel Cender <danielcender@gmail.com>
2019-03-15 20:29:33 -0500

    chore: added root index.php file

---
+++ index.php
@@ -0,0 +1,24 @@
+<!--
+/*
+ * Class: CST-126-O500 @ Grand Canyon University, Spring 2019
+ * Author: Daniel Cender
+ * Date: March 10, 2019
+ * Synopsis: This HTML file is the entry point into my homework directory. It links to all my assignments.
+ */
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>CST-126-Class-Projects</title>
+</head>
+<body>
+<div>
+<h2><a href="activity1/register.html">Activity1</a></h2>
+<h2><a href="activity2/index.html">Activity2</a></h2>
+<h2><a href="activity3/index.html">Activity3</a></h2>
+<h2><a href="BloggingPlatform/index.php">Blogging Platform</a></h2>
+</div>
+</body>
+</html>
