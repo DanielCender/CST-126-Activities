3e54f3f2524bd7448ad055262bae3984690395ac
Daniel Cender <danielcender@gmail.com>
2019-03-21 19:29:23 -0500

    test: switched activity1 over to Heroku capable-ness

--- activity1/getAllUsers.php
+++ activity1/getAllUsers.php
@@ -7,19 +7,22 @@
  * Synopsis: This exports the rows of user data as an html table.
  */
 
-$user = 'root';
-$password = 'root';
-$db = 'activity1';
-$host = 'localhost';
+
+// Below -> Safely get Env Vars, per https://secure.php.net/manual/en/function.getenv.php
+$user = (getenv('CLEARDB_DATABASE_USER', true) ?: getenv('CLEARDB_DATABASE_USER')) ?: 'root';
+$password = (getenv('CLEARDB_DATABASE_PASSWORD', true) ?: getenv('CLEARDB_DATABASE_PASSWORD')) ?: 'root';
+$db = (getenv('CLEARDB_DATABASE_DB', true) ?: getenv('CLEARDB_DATABASE_DB')) ?: 'blog';
+$host = (getenv('CLEARDB_DATABASE_HOST', true) ?: getenv('CLEARDB_DATABASE_HOST')) ?: 'localhost';
+
 $port = 8889;
 
-$conn = new mysqli($host, $user, $password, $db, $port);
+$conn = new mysqli($host, $user, $password, $db);
 
 if ($conn->connect_error) {
     echo $conn->connect_error;
 }
 
-$sqlQuery = "SELECT * FROM users";
+$sqlQuery = "SELECT * FROM activity1_users";
 
 $resultSet = $conn->query($sqlQuery);
 
--- activity1/pageHandler.php
+++ activity1/pageHandler.php
@@ -8,11 +8,14 @@
  */
 $firstName = $lastName = "";
 
-$user = 'root';
-$password = 'root';
-$db = 'activity1';
-$host = 'localhost';
+$user = (getenv('CLEARDB_DATABASE_USER', true) ?: getenv('CLEARDB_DATABASE_USER')) ?: 'root';
+$password = (getenv('CLEARDB_DATABASE_PASSWORD', true) ?: getenv('CLEARDB_DATABASE_PASSWORD')) ?: 'root';
+$db = (getenv('CLEARDB_DATABASE_DB', true) ?: getenv('CLEARDB_DATABASE_DB')) ?: 'blog';
+$host = (getenv('CLEARDB_DATABASE_HOST', true) ?: getenv('CLEARDB_DATABASE_HOST')) ?: 'localhost';
+
 $port = 8889;
+
+$conn = new mysqli($host, $user, $password, $db);
 
 if (empty($_POST['firstName'])) {
     echo "The First Name is a required field and cannot be blank.";
@@ -22,13 +25,13 @@ if (empty($_POST['firstName'])) {
     $firstName = htmlspecialchars($_POST['firstName']);
     $lastName = htmlspecialchars($_POST['lastName']);
     // Open MySQL connection
-    $conn = new mysqli($host, $user, $password, $db, $port);
+    $conn = new mysqli($host, $user, $password, $db);
     
     if ($conn->connect_error) {
         echo $conn->connect_error;
     }
     
-    $sqlInsert = "INSERT INTO users (FIRST_NAME, LAST_NAME) VALUES('$firstName', '$lastName')";
+    $sqlInsert = "INSERT INTO activity1_users (FIRST_NAME, LAST_NAME) VALUES('$firstName', '$lastName')";
     
     if ($conn->query($sqlInsert) === TRUE) {
         echo "First Name is " . $firstName . "<br>";
