9fe10d3640a932b6f96301d4bd1e9b878d75a153
Daniel Cender <danielcender@gmail.com>
2019-04-07 12:08:43 -0500

    feat: basic viewPost.php page

--- BloggingPlatform/modules/post/viewPost.php
+++ BloggingPlatform/modules/post/viewPost.php
@@ -11,60 +11,56 @@ require_once '../helpers/session.php';
 require_once '../helpers/db.php';
 
 $userId = getUserId();
-$postId = $_GET["postId"];
+
+$postId = $_GET["id"];
 
 $conn = dbConnect();
-$post = $conn->query("SELECT * FROM post WHERE ID = $postId");
+$post = $conn->query("SELECT post.ID,post.Title,post.Content,post.Votes,CONCAT(user.FirstName, ' ',user.LastName) AS Author FROM post INNER JOIN user WHERE post.Author = user.ID AND post.ID = $postId");
 
 $row = $post->fetch_assoc();
 
 $title = $row["Title"];
 $content = $row["Content"];
+$author = $row["Author"];
+$votes = $row["Votes"];
+
 $conn->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Edit Post</title>
+<title>View Post</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 <script src="../helpers/languageFilter.js"></script>
 
-<!--Import Google Icon Font-->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-	rel="stylesheet">
-<!--Import materialize.css-->
-<link type="text/css" rel="stylesheet" href="../../css/materialize.min.css"
-	media="screen,projection" />
-
-<!--Let browser know website is optimized for mobile-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 </head>
 <body>
-<?php include('../header/_header.php'); ?>
-<form action="post.php" method="POST">
-	
-	<div class="form_comp">
-<div class="form_comp__section">
-<label>Title</label>
-<input type="text" name="title" value="<?php echo $title; ?>" onchange="languageFilter(this.value)">
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-3"></div>
+		<h1 class="display-3 col-md-6 text-center"><?php echo $title; ?></h1>
+		<div class="col-md-3"></div>
+	</div>
+	<div class="row">
+	<div class="col-md-3"></div>
+		<h4 class="col-md-6 text-center">by <?php echo $author; ?></h4>
+		<div class="col-md-3"></div>
+	</div>
+	<div>
+		<div class="col-md-3"></div>
+		<p class="col-md-6 text-left"><?php echo $content; ?></p>
+		<div class="col-md-3"></div>
+	</div>
 </div>
 
-<div class="form_comp__section">
-<label>Content</label>
-<input type="text" name="content" value="<?php echo $content; ?>" onchange="languageFilter(this.value)">
-</div>
-<input type="hidden" name="author" value="<?php echo $userId; ?>">
-<input type="hidden" name="id" value="<?php echo $postId; ?>">
-
-<button id="submit_btn" class="btn_submit" type="submit" name="action" value="update">Update</button>
-<button class="btn_submit" type="submit" name="action" value="delete">Delete</button>
-</div>
-</form>
-
-<!--JavaScript at end of body for optimized loading-->
-	<script type="text/javascript" src="../../js/materialize.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
